20f5034ca4c93db41891e889924c5dd4eb634eb9
Yato Aki <yatoakii@gmail.com>
2022-05-10 13:42:25 +0630

    Added User db, model and controller

--- db/migrate/20220510062535_create_subs.rb
+++ db/migrate/20220510062535_create_subs.rb
@@ -1,8 +1,8 @@
 class CreateSubs < ActiveRecord::Migration[7.0]
   def change
     create_table :subs do |t|
-      t.string :name
-      t.text :detail
+      t.string :name, null: false
+      t.text :detail, null: false
 
       t.timestamps
     end
