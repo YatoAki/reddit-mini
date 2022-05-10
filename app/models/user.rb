07a0054a92491b7302669c80615a3cae3c02c850
Yato Aki <yatoakii@gmail.com>
2022-05-10 13:56:34 +0630

    Added User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ApplicationRecord
+  after_initialize :ensure_session_token
+
+  validates :username, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+
+  validates :password_digest, presence: {message: ", Password cannot be blank"}
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader :password
+
+  def self.find_by_credentials(username,password)
+    user = User.all.find_by_username(username)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(@password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64(16)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_10_062535) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_10_071030) do
   create_table "subs", force: :cascade do |t|
     t.string "name"
     t.text "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
