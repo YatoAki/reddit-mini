82cc5e1f39d378eebe702d93cfb00f947d97a587
Yato Aki <yatoakii@gmail.com>
2022-05-10 14:07:29 +0630

    New user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login_user!(@user)
+      redirect_to subs_url
+    else
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:password,:session_token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   root "subs#index"
 
   resources :subs
+  resources :users
 end
