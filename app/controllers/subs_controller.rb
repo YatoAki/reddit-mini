263659ac435aa459779a601d8a05a632e42b8c84
Yato Aki <yatoakii@gmail.com>
2022-05-10 13:26:19 +0630

    Create sub

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -8,4 +8,43 @@ class SubsController < ApplicationController
     @sub = Sub.find(params[:id])
   end
 
+  def new
+    @sub = Sub.new
+  end
+
+  def create
+    @sub = Sub.new(sub_params)
+    if @sub.save
+      redirect_to @sub
+    else
+      render :new,status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @sub = Sub.find(params[:id])
+  end
+
+  def update
+    @sub = Sub.find(params[:id])
+    if @sub.update(sub_params)
+      redirect_to @sub
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @sub = Sub.find(params[:id])
+    @sub.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def sub_params
+    params.require(:sub).permit(:name,:detail)
+  end
+
 end
